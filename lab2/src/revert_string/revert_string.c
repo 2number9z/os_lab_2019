dc2a0fcb1d1403c6bdc20c706ab687d3eb14b57d
2number9z <ogru123@gmail.com>
2019-11-09 16:47:53 +0000

    1,2,3 задания

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -3,5 +3,14 @@
 void RevertString(char *str)
 {
 	// your code here
+	char *reversed = (char*)malloc(sizeof(char) * (strlen(str) + 1));
+	int i = strlen(str)-1;
+	for (; i > -1; i--) {
+
+		*(reversed + (strlen(str) - i) - 1) = *(str + i);
+	}
+	*(reversed + strlen(str)) = '\0';
+	strcpy(str, reversed);
+	free(reversed);
 }
 
