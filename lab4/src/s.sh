95cb6a6a90d3f62baab68671fa0446ca3e19c6bd
2number9z <ogru123@gmail.com>
2019-11-13 21:12:58 +0000

    zadanie 1

---
+++ lab4/src/s.sh
@@ -0,0 +1,3 @@
+rm qwe
+gcc parallel_min_max.c /projects/2number9z-os_lab_2019/lab3/src/utils.c /projects/2number9z-os_lab_2019/lab3/src/find_min_max.c -o qwe
+./qwe --seed 1 --array_size 25000000 --pnum 4 --timeout 2
