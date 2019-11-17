ea450b4dc9fe41452cb8cd50800ecfcf1a6b0be7
2number9z <ogru123@gmail.com>
2019-11-17 23:32:43 +0000

    done

--- lab6/src/client.c
+++ lab6/src/client.c
@@ -13,6 +13,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include "libmult.h"
+
 struct Server {
   char ip[255];
   int port;
@@ -28,18 +30,18 @@ struct ThreadArgs{
 int result = 1;
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
+// uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
+//   uint64_t result = 0;
+//   a = a % mod;
+//   while (b > 0) {
+//     if (b % 2 == 1)
+//       result = (result + a) % mod;
+//     a = (a * 2) % mod;
+//     b /= 2;
+//   }
 
-  return result % mod;
-}
+//   return result % mod;
+// }
 
 bool ConvertStringToUI64(const char *str, uint64_t *val) {
   char *end = NULL;
